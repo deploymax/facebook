eb237d7e16c607a0ac2439409db56fbf768a7e23
deploymax <deploymax@gmail.com>
2022-08-22 15:16:29 +0530

    2nd Commit

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+server '10.172.234.113', user: 'deploy', roles: %w{app db web}
 
 # role-based syntax
 # ==================
